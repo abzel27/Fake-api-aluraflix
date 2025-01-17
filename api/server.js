28a2c775d1f3ccf1d7454dc60178fe254e534b51
Abdiel Castilllo Zeledon <145421129+abzel27@users.noreply.github.com>
2025-01-17 14:29:31 -0600

    Update server.js
    volvio al codigo original

--- api/server.js
+++ api/server.js
@@ -3,18 +3,15 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
-
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/cards/:id/show': '/cards/:id', // Acceso directo a una tarjeta por ID
-    '/categories/:id/show': '/categories/:id' // Acceso directo a una categoría por ID
-}));
+    '/product/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
-
 // Export the Server API
 module.exports = server
